69d00ab8c6f5c4452f84394e93d6f1cab598631e
Shreek195 <shreekoshti199@gmail.com>
2026-06-25 08:59:26 +0530

    added substraction

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
 int1 = int(input("Enter first No. : "))
 int2 = int(input("Enter first No. : "))
 
-print("Addition of Given No. : ", int1+int2)
+print("Addition of Given No. : ", int1 + int2)
 
-print("Multiplication :",int1*int2)
+print("Multiplication :", int1 * int2)
+
+print("substraction :", int1 - int2)
